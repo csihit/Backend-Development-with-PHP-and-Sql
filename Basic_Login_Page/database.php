7bb8caa5f17d8fc11e2b205725efc6e1c99bdec4
CSI (Hit Student's Branch) <160616977+csihit@users.noreply.github.com>
2024-04-16 16:25:33 +0530

    Update database.php

--- Basic_Login_Page/database.php
+++ Basic_Login_Page/database.php
@@ -3,17 +3,17 @@
     $dbserver = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "businessdb";
-    $conn = "";
-
-    try {
+    $db_name = "login_info";
+    
+    $conn = mysqli_connect($dbserver, $db_user, $db_pass, $db_name);
+    /*try {
         $conn = mysqli_connect($dbserver, $db_user, $db_pass, $db_name);
         // $conn = mysqli_connect("localhost", "root", "", "businessdb");
         echo "Database connection established successfully";
     }
     catch (mysqli_sql_exception) {
         echo "Connection failed";
-    }
+    }*/
     
     // if($conn) {
     //     echo "Connection established successfully";
